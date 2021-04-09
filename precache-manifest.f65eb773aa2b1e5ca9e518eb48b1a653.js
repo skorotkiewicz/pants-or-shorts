61358a779d3d3a77463161b82e887a83ead577d4
skorotkiewicz <skorotkiewicz@users.noreply.github.com>
2021-04-09 22:10:15 +0000

    deploy: edc3de7d9c14825e9274a9cd29ad2837b8f3c827

---
+++ precache-manifest.f65eb773aa2b1e5ca9e518eb48b1a653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b416b395dc55b9b123ac956ad265ce2",
+    "url": "/pants-or-shorts/index.html"
+  },
+  {
+    "revision": "9e701ed21fa964e6dda6",
+    "url": "/pants-or-shorts/static/css/main.7168c233.chunk.css"
+  },
+  {
+    "revision": "a8399321e48b173796a4",
+    "url": "/pants-or-shorts/static/js/2.1e8c7bb0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pants-or-shorts/static/js/2.1e8c7bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e701ed21fa964e6dda6",
+    "url": "/pants-or-shorts/static/js/main.a9edeba8.chunk.js"
+  },
+  {
+    "revision": "ceccdc33f22a687deaa9",
+    "url": "/pants-or-shorts/static/js/runtime-main.19e2ec13.js"
+  }
+]);
